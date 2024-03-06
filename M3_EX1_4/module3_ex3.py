4ed02e7b5766fb864cfaea1d15198d85ed468194
CrazyCatParis <159175018+CrazyCatParis@users.noreply.github.com>
2024-03-06 16:01:45 +0100

    Update module3_ex3.py

--- M3_EX1_4/module3_ex3.py
+++ M3_EX1_4/module3_ex3.py
@@ -10,21 +10,22 @@ raw_numbers = [
     "(050)8889900",
     "38050-111-22-22",
     "38050 111 22 11   ",
+    "+33 4 78 90 12 34",
 ]
 
-def normalize_phone(num):
-    pattern = r'\D'
-    replacement = r''
-    formatted_number = re.sub(pattern, replacement, num)
-    if formatted_number[0] == '+':
-        pass
-    if formatted_number[:2] == '38':
-        formatted_number = '+' + formatted_number
-    elif formatted_number[0] != '+':
-        formatted_number = '+38' + formatted_number
+def normalize_phone(phone_number):
+    # Видаляємо всі символи, крім цифр та символу '+'
+    formatted_number = re.sub(r'[^\d+]', '', phone_number)
+    # Додаємо міжнародний код, якщо потрібно
+    if not formatted_number.startswith('+'):
+        if formatted_number.startswith('380'):
+            formatted_number = '+' + formatted_number
+        else:
+            formatted_number = '+38' + formatted_number
     return formatted_number
-
 
     
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+
+    
