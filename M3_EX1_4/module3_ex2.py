6e1b0426f8807707b8c5d6be3182e75a9bd03c7a
CrazyCatParis <159175018+CrazyCatParis@users.noreply.github.com>
2024-03-06 19:44:06 +0100

    Update module3_ex2.py

--- M3_EX1_4/module3_ex2.py
+++ M3_EX1_4/module3_ex2.py
@@ -1,14 +1,21 @@
 import random
 
-def get_numbers_tickets(min, max, quantity):
-  numbers = set()
-  while len(numbers) < quantity:
-    numbers.add(random.randint(min, max))
-  return sorted(list(numbers))
+import random
 
-min = 1  
-max = 49
-quantity = 6
-numbers = get_numbers_tickets(min, max, quantity)
-print(f'Your lottery numbers are: {numbers}')
+min = input("Enter the minimum number between 1 and 1000: ")
+max = input("Enter the maximum number between 1 to 1000: ")
+quantity = input("Enter the number of numbers in the winning combination: ")
 
+def get_numbers_ticket(min, max, quantity):
+    try:
+        min = int(min)
+        max = int(max)
+        quantity = int(quantity)
+        if min < 1 or max > 1000 or min>=max or quantity < 1 or quantity > (max - min + 1):
+          print("Invalid parameters.")
+          return []
+        return sorted(random.sample(list(range( min, max + 1)), quantity))
+    except Exception:
+        print("Please enter whole numbers")
+        exit()
+print("Winning numbers", get_numbers_ticket(min, max, quantity))
