95301217cd205d74d1d526443857190f2e4416f0
CrazyCatParis <159175018+CrazyCatParis@users.noreply.github.com>
2024-03-07 20:34:30 +0100

    Update module3_ex1.py

--- M3_EX1_4/module3_ex1.py
+++ M3_EX1_4/module3_ex1.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 
-def input_function():
-   while True:
-       try:
-           input_date = input('Please enter date in format YYYY-MM-DD: ')
-           input_datetime = datetime.strptime(input_date, "%Y-%m-%d")
-           break
-       except ValueError:
-           print('Incorrect data format, should be YYYY-MM-DD')
-   return input_datetime
-
+def date():
+    while True:
+        try:
+            input_date = input('Please enter date in format YYYY-MM-DD: ')
+            input_datetime = datetime.strptime(input_date, "%Y-%m-%d")
+            break
+        except ValueError:
+            print('Incorrect data format, should be YYYY-MM-DD')
+    return input_datetime
 
 def get_days_from_today(input_datetime_paramenter):
     today = datetime.today()
@@ -17,4 +16,5 @@ def get_days_from_today(input_datetime_paramenter):
     print(f'Your date is {difference} days from today')
     return difference
 
-get_days_from_today(input_function())
+get_days_from_today(date())
+
